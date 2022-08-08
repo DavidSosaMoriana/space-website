b84f879990984133255f7409cd10f93843beb5b3
DavidSosaMoriana <sosamorianadav@gmail.com>
2022-08-08 22:18:00 +0100

    :art: homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Header } from './components';
+import { Home, Header } from './components';
 import { Destination, Crew, Technology } from './pages';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
         <Router>
             <Header />
             <Routes>
-                <Route path="/" exact></Route>
+                <Route path="/" element={<Home />}></Route>
                 <Route path="/Destination" element={<Destination />}></Route>
                 <Route path="/Crew" element={<Crew />}></Route>
                 <Route path="/Technology" element={<Technology />}></Route>
---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <>
+            <section className="home">
+                <div className="overlay">
+                    <h1>
+                        SO, YOU WANT TO TRAVEL TO <span>SPACE</span>
+                    </h1>
+                </div>
+            </section>
+        </>
+    );
+}
