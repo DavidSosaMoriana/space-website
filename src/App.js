0e2693fe12bf84f045e48a23dd010bc0af27255e
DavidSosaMoriana <sosamorianadav@gmail.com>
2022-08-08 22:03:34 +0100

    :adhesive_bandage route path

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components';
-import { Destination, Crew, Technology } from './pages'
+import { Destination, Crew, Technology } from './pages';
 
 function App() {
     return (
@@ -8,15 +8,9 @@ function App() {
             <Header />
             <Routes>
                 <Route path="/" exact></Route>
-                <Route path="/Destination" exact>
-                  <Destination />
-                </Route>
-                <Route path="/Crew" exact>
-                  <Crew />
-                </Route>
-                <Route path="/Technology" exact>
-                  <Technology />
-                </Route>
+                <Route path="/Destination" element={<Destination />}></Route>
+                <Route path="/Crew" element={<Crew />}></Route>
+                <Route path="/Technology" element={<Technology />}></Route>
             </Routes>
         </Router>
     );
