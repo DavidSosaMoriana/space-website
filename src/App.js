2fb8ef6345cb5cb2f5f163918a9907954ca61bc6
DavidSosaMoriana <sosamorianadav@gmail.com>
2022-08-08 21:34:56 +0100

    @PAGES navigation items

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components';
+import { Destination, Crew, Technology } from './pages'
 
 function App() {
     return (
@@ -7,6 +8,15 @@ function App() {
             <Header />
             <Routes>
                 <Route path="/" exact></Route>
+                <Route path="/Destination" exact>
+                  <Destination />
+                </Route>
+                <Route path="/Crew" exact>
+                  <Crew />
+                </Route>
+                <Route path="/Technology" exact>
+                  <Technology />
+                </Route>
             </Routes>
         </Router>
     );
